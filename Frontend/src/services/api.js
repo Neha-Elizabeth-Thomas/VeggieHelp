e10fd0cd536848225b10391e76260485a4af0d2c
Neha Elizabeth Thomas <nehaelizabeththomas0@gmail.com>
2025-08-09 06:32:32 +0530

    updated

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const api = axios.create({
   // The baseURL will be read from the .env file.
   // The Vite proxy will handle forwarding this to your backend server.
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   // This is crucial for sending the httpOnly JWT cookie back and forth
   withCredentials: true, 
 });
