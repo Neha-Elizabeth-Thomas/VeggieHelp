5874899ede252558d364f13e44a7f03115d4db71
Neha Elizabeth Thomas <nehaelizabeththomas0@gmail.com>
2025-08-09 09:08:13 +0530

    updated

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -8,8 +8,8 @@ const generateToken = (res, userId) => {
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-        sameSite: 'strict', // Prevent CSRF attacks
+        secure: process.env.NODE_ENV !== 'development', // true in production
+        sameSite: process.env.NODE_ENV !== 'development' ? 'None' : 'strict', // 'None' in production
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 };
